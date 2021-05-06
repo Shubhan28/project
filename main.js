4a8e568a5ee5cfacb035714618c5d9bef4ed3418
Shubhan28 <83396802+Shubhan28@users.noreply.github.com>
2021-05-06 14:26:54 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,44 +1,26 @@
+eyesX = 0;
+eyesY = 0;
+noseX = 0;
+noseY = 0;
+mouthX = 0;
+mouthY = 0
 function preload()
 {
-
+   specs = loadImage('https://clipground.com/images/specks-clipart-12.jpg');
+   mustach = loadImage('https://i.postimg.cc/3x3QzSGq/m.png');
+   lips = loadImage('https://i.postimg.cc/PxFvYgkv/l1.png');
+   nose_image = loadImage('https://i2.wp.com/freepngimages.com/wp-content/uploads/2019/03/red-nose.png?fit=800%2C800');
 }
 function setup()
 {
     canvas = createCanvas(300, 300);
-    video = captureVideo(VIDEO);
+    video = createCapture(VIDEO);
+    video.size(300, 300);
     video.hide();
+    classifier = ml5.poseNet(video, modelLoaded);
+    classifier.on('pose', gotposes);
 }
 function draw()
 {
-    fill(0, 128, 0);
-    stroke(0, 128, 0);
-    circle(0, 128, 0, 70);
-
-    fill(0, 128, 0);
-    stroke(0, 128, 0);
-    circle(0, 200, 0, 70);
-
-    fill(0, 128, 0);
-    stroke(0, 128, 0);
-    rect(0, 300, 0, 70);
-
-    fill(0, 128, 0);
-    stroke(0, 128, 0);
-    rect(0, 400, 0, 70);
-
-    fill(0, 128, 0);
-    stroke(0, 128, 0);
-    circle(0, 500, 0, 70);
-
-    fill(0, 128, 0);
-    stroke(0, 128, 0);
-    rect(0, 600, 0, 70);
-
-    fill(0, 128, 0);
-    stroke(0, 128, 0);
-    circle(0, 700, 0, 70);
-
-    fill(0, 128, 0);
-    stroke(0, 128, 0);
-    rect(0, 800, 0, 70);
+   image(video, 0, 0, 300, 300);
 }
